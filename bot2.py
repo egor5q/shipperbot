e6ce9a206228857af84d2ef81cd5df7183c43416
egor5q <31898567+egor5q@users.noreply.github.com>
2018-10-16 14:11:30 +0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,1 +1,26 @@
+# -*- coding: utf-8 -*-
+import os
+import telebot
+import time
+import telebot
+import random
+import info
+import threading
+from emoji import emojize
+from telebot import types
+from pymongo import MongoClient
+from emoji import emojize
 
+
+token = os.environ['TELEGRAM_TOKEN']
+bot = telebot.TeleBot(token)
+
+
+
+
+
+
+
+if True:
+    bot.polling(none_stop=True)
+  
